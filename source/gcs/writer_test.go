ac4e278792f5326c991b8bae1e4f39f24c3cdb60
Hang Xie <7977860+hangxie@users.noreply.github.com>
2025-09-20 18:05:06 -0700

    refactor gcs unti tests

--- source/gcs/writer_test.go
+++ source/gcs/writer_test.go
@@ -5,50 +5,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/api/option"
 
 	"cloud.google.com/go/storage"
 
 	"github.com/hangxie/parquet-go/v2/source"
 )
 
-func Test_GcsFileExternalClientFlag(t *testing.T) {
-	ctx := context.Background()
-
-	file := gcsFile{
-		ctx:            ctx,
-		gcsClient:      &storage.Client{},
-		projectID:      "test-project",
-		bucketName:     "test-bucket",
-		filePath:       "test.parquet",
-		externalClient: true,
-	}
-
-	require.True(t, file.externalClient)
-
-	file.externalClient = false
-	require.False(t, file.externalClient)
-}
-
 func Test_GcsFileInterfaceComplianceWriter(t *testing.T) {
 	var _ source.ParquetFileWriter = (*gcsFileWriter)(nil)
-}
-
-func Test_GcsFileStructure(t *testing.T) {
-	ctx := context.Background()
-
-	file := gcsFile{
-		projectID:      "test-project",
-		bucketName:     "test-bucket",
-		filePath:       "test.parquet",
-		gcsClient:      &storage.Client{},
-		externalClient: true,
-		ctx:            ctx,
-	}
-
-	require.Equal(t, "test-project", file.projectID)
-	require.Equal(t, "test-bucket", file.bucketName)
-	require.Equal(t, "test.parquet", file.filePath)
-	require.True(t, file.externalClient)
 }
 
 func Test_GcsWriterCreate(t *testing.T) {
@@ -93,4 +58,13 @@ func Test_GcsWriterStructure(t *testing.T) {
 	require.Equal(t, "test-project", writer.projectID)
 	require.Equal(t, "test-bucket", writer.bucketName)
 	require.Equal(t, "test.parquet", writer.filePath)
+}
+
+func Test_NewGcsFileWriterWithClient(t *testing.T) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx, option.WithoutAuthentication())
+	require.NoError(t, err)
+	_, err = NewGcsFileWriterWithClient(ctx, client, "test-project", "test-bucket", "test.parquet")
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to close writer: googleapi: Error 401")
 }
