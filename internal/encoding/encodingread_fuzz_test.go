7d12fa9156c06ae667aa4e3c4841e6f87db67ec2
Hang Xie <7977860+hangxie@users.noreply.github.com>
2026-06-24 14:37:15 -0700

    test(fuzz): add fuzz tests for encoding decoders

---
+++ internal/encoding/encodingread_fuzz_test.go
@@ -0,0 +1,30 @@
+package encoding
+
+import (
+	"bytes"
+	"testing"
+)
+
+func FuzzReadRLEBitPackedHybrid(f *testing.F) {
+	f.Add([]byte{}, uint(1))
+	// RLE run: header=8 (4 repeats), value byte 0x00
+	f.Add([]byte{0x08, 0x00}, uint(1))
+	// bit-packed run: header=3 (1 group of 8), 1 byte of data
+	f.Add([]byte{0x03, 0xFF}, uint(1))
+
+	f.Fuzz(func(t *testing.T, data []byte, bitWidth uint) {
+		// bitWidth is bounded to a realistic range; length tracks the input
+		// size so the decoder consumes only what the fuzzer provided.
+		_, _ = ReadRLEBitPackedHybrid(bytes.NewReader(data), uint64(bitWidth%65), uint64(len(data)))
+	})
+}
+
+func FuzzReadBitPackedCount(f *testing.F) {
+	f.Add([]byte{}, uint(0), uint(0))
+	f.Add([]byte{0xFF}, uint(8), uint(1))
+	f.Add([]byte{0xFF, 0xFF}, uint(8), uint(2))
+
+	f.Fuzz(func(t *testing.T, data []byte, cnt, bitWidth uint) {
+		_, _ = ReadBitPackedCount(bytes.NewReader(data), uint64(cnt%4096), uint64(bitWidth%65))
+	})
+}
